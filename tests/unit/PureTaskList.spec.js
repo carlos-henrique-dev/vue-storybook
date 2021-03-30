8e7516d6feb1e981399ee6fe0578442f2d488fe6
henrique <henriqueok20@gmail.com>
2021-03-30 14:30:36 -0400

    finishes vuex integration

--- tests/unit/PureTaskList.spec.js
+++ tests/unit/PureTaskList.spec.js
@@ -1,11 +1,11 @@
 import Vue from "vue";
 
-import TaskList from "../../src/components/TaskList.vue";
+import PureTaskList from "../../src/components/PureTaskList.vue";
 
-import { WithPinnedTasks } from "../../src/components/Tasklist.stories";
+import { WithPinnedTasks } from "../../src/components/PureTaskList.stories";
 
 it("Renders pinned Tasks at the start of the listI", () => {
-  const Constructor = Vue.extend(TaskList);
+  const Constructor = Vue.extend(PureTaskList);
 
   const vm = new Constructor({
     propsData: WithPinnedTasks.args
